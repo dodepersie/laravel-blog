7fcd9d275e06a1341608b52f435e6cb92446ffbf
dodepersie <dodepersie@gmail.com>
2023-08-24 12:29:58 +0800

    Share Toggle

--- public/js/share-toggle.js
+++ public/js/share-toggle.js
@@ -1,26 +1,55 @@
-$(document).ready(function() {
-    var shareButtonShow = $("#share_button_show");
+// $(document).ready(function() {
+//     var shareButtonShow = $("#share_button_show");
 
-    $("#share_button").on("click", function() {
-        shareButtonShow.slideToggle();
+//     $("#share_button").on("click", function() {
+//         shareButtonShow.slideToggle();
+
+//         // Toggle body's overflow property to disable/enable scrolling
+//         $("body").css("overflow", function(index, value) {
+//             return value === "hidden" ? "auto" : "hidden";
+//         });
+//     });
+
+//     // Hide the dropdown and re-enable scrolling when clicking outside
+//     $(document).on("click", function(e) {
+//         if (!$(e.target).closest("#share_button").length && !$(e.target).closest(
+//                 "#share_button_show").length) {
+//             shareButtonShow.slideUp();
+//             $("body").css("overflow", "auto");
+//         }
+//     });
+
+//     // Prevent the dropdown from closing when clicking inside
+//     shareButtonShow.on("click", function(e) {
+//         e.stopPropagation();
+//     });
+// });
+
+$(document).ready(function () {
+    $("#share_button").click(function (e) {
+        e.stopPropagation(); // Prevent the click from propagating to the document
+
+        var shareButtonShow = $(this).next("#share_button_show");
+
+        // Hide other share_button_show elements and toggle the clicked one
+        $("#share_button_show").not(shareButtonShow).slideUp();
 
         // Toggle body's overflow property to disable/enable scrolling
-        $("body").css("overflow", function(index, value) {
+        $("body").css("overflow", function (index, value) {
             return value === "hidden" ? "auto" : "hidden";
         });
+        shareButtonShow.slideToggle();
     });
 
     // Hide the dropdown and re-enable scrolling when clicking outside
-    $(document).on("click", function(e) {
-        if (!$(e.target).closest("#share_button").length && !$(e.target).closest(
-                "#share_button_show").length) {
+    $(document).on("click", function (e) {
+        var shareButtonShow = $("#share_button_show");
+        if (
+            !$(e.target).closest("#share_button").length &&
+            !$(e.target).closest("#share_button_show").length
+        ) {
             shareButtonShow.slideUp();
             $("body").css("overflow", "auto");
         }
-    });
-
-    // Prevent the dropdown from closing when clicking inside
-    shareButtonShow.on("click", function(e) {
-        e.stopPropagation();
     });
 });
